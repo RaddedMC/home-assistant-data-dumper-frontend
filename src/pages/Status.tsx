6bdae672b54b87a4fcfa3395c23f5622f1ad003c
RaddedMC <mycooljames@hotmail.com>
2026-08-21 22:58:03 -0400

    .

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -339,6 +339,7 @@ export default function Status(props: MobileProps) {
                 </div>
             </div>
 
+            {/* // TODO: Make this prettier */}
             <span className=" text-center w-full text-gray-600 mt-8">Note: queued and scheduled task history is cleared when the app is restarted.</span>
             <ExportButton/>
         </>
